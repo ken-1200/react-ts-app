2c3060955e8225153eb78e61115aefb55d9c848b
ken-1200 <kensuke19971022@gmail.com>
2021-06-25 23:33:51 +0900

    feat: 構成更新

---
+++ src/components/Task.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Task() {
+  return (
+  <React.Fragment>
+    <h1>task list</h1>
+  </React.Fragment>
+  );
+}
+
+export default Task;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Task from './components/Task';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Task />
   </React.StrictMode>,
   document.getElementById('root')
 );
