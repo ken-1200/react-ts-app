f29adedc6248adfdcb733850c8bdcc05862a97fb
ken-1200 <kensuke19971022@gmail.com>
2021-06-28 23:12:37 +0900

    feat: NewTaskコンポーネント・作成処理、テスト実装

--- src/__tests__/NewTask.test.tsx
+++ src/__tests__/NewTask.test.tsx
@@ -1,15 +1,19 @@
+import axios from 'axios';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { createMemoryHistory } from 'history';
 
 import { shallow } from 'enzyme';
 import NewTask from '../components/organisms/NewTask';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('InputText.tsx', () => {
+jest.mock('axios');
+
+describe('NewTask.tsx', () => {
   it('changeParentTitleを呼び出すと、setStateが呼び出されること', () => {
     // == 準備 ==
-    /** Appコンポーネントをshallowレンダリング */
+    /** NewTaskコンポーネントをshallowレンダリング */
     const wrapper: any = shallow(<NewTask />);
 
     /** setStateしてthis.state.inputValueの値を'XXX'に更新 */
@@ -26,5 +30,34 @@ describe('InputText.tsx', () => {
     // == 検証 ==
     /** 適切な引数でspy化したsetStateが呼び出されていればOK */
     expect(wrapper.state()).toStrictEqual({ title: 'BBB', text: 'BBB' });
+  });
+
+  afterEach(() => jest.restoreAllMocks());
+
+  it('postTaskを呼び出すと、タスクがpostされ、ホーム画面に遷移すること', async () => {
+    // == 準備 ==
+    /** NewTaskコンポーネントをshallowレンダリング */
+    const wrapper: any = shallow(<NewTask />);
+
+    /** createMemoryHistoryで履歴が実際に変更されたことを確認する */
+    const history = createMemoryHistory();
+
+
+    /** axios postの設定 */
+    const payload = {title: "title", text: "text"};
+    (axios.post as any).mockResolvedValue(payload);
+
+    // == 実行 ==
+    /** historyHome() */
+    history.push('/');
+
+    /** postTask() */
+    const data = await wrapper.instance().postTask();
+
+  
+    // == 検証 ==
+    /** 適切な引数でspy化したsetStateが呼び出されていればOK */
+    expect(data).toBe(payload);
+    expect(history.location.pathname).toBe('/');
   });
 });
--- src/components/organisms/NewTask.tsx
+++ src/components/organisms/NewTask.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { TitleForm } from "../molecules/TitleForm";
 import { TextForm } from "../molecules/TextForm";
+import { SubmitButton } from "../atoms/buttons/SubmitButton";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-type State = {
+interface State {
   title: string;
   text: string;
 }
 
-class NewTask extends React.Component<{}, State, any> {
+class NewTask extends React.Component<any, State, any> {
   // 初期化
   public constructor(props: any) {
     super(props);
@@ -18,6 +21,7 @@ class NewTask extends React.Component<{}, State, any> {
 
     this.changeParentTitle = this.changeParentTitle.bind(this);
     this.changeParentText = this.changeParentText.bind(this);
+    this.clickSubmitButton = this.clickSubmitButton.bind(this);
   }
 
   // 受け取った引数をステートに格納
@@ -29,12 +33,43 @@ class NewTask extends React.Component<{}, State, any> {
     this.setState({ text: value });
   }
 
+  private async clickSubmitButton(): Promise<void> {
+    // Post実行
+    await this.postTask();
+
+    // 画面遷移
+    this.historyHome();
+  }
+
+  private async postTask() {
+    const url: string = `${process.env.REACT_APP_BSSE_URL}/todos`;
+    const payload = {
+      title: this.state.title,
+      text: this.state.text,
+    };
+
+    // 作成API
+    return await axios
+      .post(url, payload)
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  private historyHome(): void {
+    // 画面遷移
+    this.props.history.push("/");
+  }
+
   render() {
     return (
       <React.Fragment>
-        <TitleForm changeParentTitle= { this.changeParentTitle } />
-        <br />
-        <TextForm changeParentText={ this.changeParentText } />
+        <div><TitleForm changeParentTitle= { this.changeParentTitle } /></div>
+        <div><TextForm changeParentText={ this.changeParentText } /></div>
+        <div>
+          <SubmitButton clickSubmitButton={ this.clickSubmitButton } />
+          <Link to="/">Cancel</Link>
+        </div>
       </React.Fragment>
     )
   }
